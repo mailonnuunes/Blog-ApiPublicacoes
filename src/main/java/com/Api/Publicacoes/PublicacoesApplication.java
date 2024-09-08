7f2ca57b06216596a13ca54167eb59f17dfe3754
Mailon Azevedo <100615338+mailonnuunes@users.noreply.github.com>
2024-09-08 11:44:33 -0300

    subindo todos arquivos iniciais

---
+++ src/main/java/com/Api/Publicacoes/PublicacoesApplication.java
@@ -0,0 +1,13 @@
+package com.Api.Publicacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Api/Publicacoes/PublicacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Api.Publicacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
